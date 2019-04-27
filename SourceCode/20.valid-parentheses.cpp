7f4c50c703350a298683020908edfaa5a549f88a
Rai95 <dream13276785298@163.com>
2019-04-27 17:48:47 +0800

    Valid Parentheses

--- SourceCode/20.valid-parentheses.cpp
+++ SourceCode/20.valid-parentheses.cpp
@@ -1,9 +1,8 @@
-﻿#include <iostream>
-#include <stack>
-#include <string>
-
-using namespace std;
-
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ */
 class Solution {
 public:
 	bool isValid(string s) {
@@ -33,10 +32,4 @@ public:
 
 	}
 };
-
-int main()
-{
-
-}
-
 
